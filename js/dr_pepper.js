aca639367d262a4d372e490106d3d638da9d6213
Yukihiro Sunaga <suna@sandpit.jp>
2015-05-03 15:38:59 +0900

    Initial commit

---
+++ js/dr_pepper.js
@@ -0,0 +1,64 @@
+
+Robot = function(isRealRobot) {
+  this.isRealRobot = isRealRobot;
+
+  this.ipAddress = "";
+  this.session   = null;
+
+  arguments.callee.actuatorList = [
+    "HeadYaw",
+    "HeadPitch",
+
+    "LShoulderPitch",
+    "LShoulderRoll",
+    "LElbowYaw",
+    "LElbowRoll",
+    "LWristYaw",
+    "LHand",
+
+    "RShoulderPitch",
+    "RShoulderRoll",
+    "RElbowYaw",
+    "RElbowRoll",
+    "RWristYaw",
+    "RHand",
+
+    "HipPitch",
+    "HipRoll",
+  ];
+};
+
+Robot.prototype.connect = function(ipAddress) {
+  if (this.ipAddress != ipAddress || this.session == null) {
+    this.ipAddress = ipAddress;
+    this.session = new QiSession(ipAddress);
+  }
+};
+
+Robot.prototype._getMemoryKey = function(actuator) {
+  return "Device/SubDeviceList/" + actuator + "/Temperature/Sensor/Value";
+};
+
+Robot.prototype.getTemperature = function(actuator, handler) {
+  if (this.isRealRobot) {
+    this.session.service("ALMemory").done(function(memory) {
+      var value = memory.getData(this._getMemoryKey(actuator));
+      handler(actuator, value);
+    }).fail(function(error) {
+	    handler(actuator, 0);
+    });
+
+  } else {
+    var host = location.hostname;
+    var url = "http://" +  host + "/dr_pepper/data/baseValue.txt";
+    $.ajax({
+      type: "GET",
+      url: url
+    }).done(function(data) {
+      var value = parseInt(data) + Math.random() * 10;
+      handler(actuator, value.toFixed(1));
+    }).error(function() {
+      handler(actuator, 0);
+    });
+  }
+};
